31c7868b67e79cc853d375dad02a165f73d5ee4f
phin01 <phin@uol.com.br>
2022-05-18 10:03:05 -0300

    Blank Scaffold

--- go_moon/lib/main.dart
+++ go_moon/lib/main.dart
@@ -1,1 +1,22 @@
 
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GoMoon App',
+      theme: ThemeData(scaffoldBackgroundColor: const Color.fromRGBO(200, 31, 31, 1.0),),
+      home: const Scaffold(
+      ),
+    );
+  }
+
+}
