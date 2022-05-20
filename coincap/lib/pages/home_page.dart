86379112c52cc28bf5dfaf35c6aa90959b443533
phin01 <phin@uol.com.br>
2022-05-20 17:10:49 -0300

    Add widgets with fixed BTC data

--- coincap/lib/pages/home_page.dart
+++ coincap/lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:coincap/models/auxiliary_functions.dart';
 import 'package:coincap/models/http_service.dart';
 import 'package:flutter/material.dart';
@@ -41,9 +43,9 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               _selectCoinDropDown(),
+              _dataWidgets(),
             ]
           ),
         ),
@@ -68,4 +70,84 @@ class _HomePageState extends State<HomePage> {
       onChanged: (_){}
       );
   }
+
+  Widget _dataWidgets() {
+    return FutureBuilder(
+      future: _httpService.get('/coins/bitcoin'),
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        if(snapshot.hasData) {
+          // full json response from API
+          Map apiData = jsonDecode(snapshot.data.toString());
+          
+          // required data for widgets
+          String imageURL = apiData['image']['large'];
+          num dollarPrice = apiData['market_data']['current_price']['usd'];
+          num dollarVariation = apiData['market_data']['price_change_percentage_24h_in_currency']['usd'];
+          String description = apiData['description']['en'];
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              _currencyImage(imageURL),
+              _currentPriceDollar(dollarPrice),
+              _currentVariationDollar(dollarVariation),
+              _currencyDescription(description)
+            ],
+          );
+        } else {
+          // waiting for API to respond, render a progress indicator
+          return const Center(child: CircularProgressIndicator(color: Colors.white,));
+        }
+      }
+    );
+  }
+
+  Widget _currentPriceDollar(num price) {
+    return Text(
+      "${price.toStringAsFixed(2)} USD",
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 30,
+      ),
+    );
+  }
+
+  Widget _currentVariationDollar(num variation) {
+    return Text(
+      "${variation.toStringAsFixed(2)}%",
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 20,
+      ),
+    );
+  }
+  
+  Widget _currencyImage(String imageURL) {
+    return Container(
+      height: _deviceHeight * 0.15,
+      width: _deviceWidth * 0.15,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: NetworkImage(imageURL))
+      ),
+    );
+  }
+
+  Widget _currencyDescription(String description) {
+    return Container(
+      height: _deviceHeight * 0.5,
+      width: _deviceWidth * 0.9,
+      padding: EdgeInsets.symmetric(vertical: _deviceHeight * 0.02, horizontal: _deviceWidth * 0.02),
+      margin: EdgeInsets.symmetric(vertical: _deviceHeight * 0.02, horizontal: _deviceWidth * 0.02),
+      color: const Color.fromRGBO(83, 88, 206, 0.6),
+      child: Text(
+        description,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 12,
+        ),
+      ),
+    );
+  }
+
 }
