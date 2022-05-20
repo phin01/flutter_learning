ebd2f7a5672bf6a702015a7b19d719cea67f5e82
phin01 <phin@uol.com.br>
2022-05-20 15:30:57 -0300

    Create get method in HttpService

--- coincap/lib/models/http_service.dart
+++ coincap/lib/models/http_service.dart
@@ -15,4 +15,16 @@ class HttpService {
     _appConfig = GetIt.instance.get<AppConfig>();
     _baseURL = _appConfig.COIN_API_BASE_URL;
   }
+
+  Future<Response?> get(String path) async {
+    try {
+      String url = "$_baseURL$path";
+      Response response = await dio.get(url);
+      return response;
+    } on DioError catch(e) {
+      return e.response;
+    } catch (e) {
+      return null;
+    }
+  }
 }
