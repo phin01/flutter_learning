68317ae82dfa644a7237ecce6eb4fc0bd8c4e295
phin01 <phin@uol.com.br>
2022-05-20 14:15:59 -0300

    Load AppConfig at startup

--- coincap/lib/main.dart
+++ coincap/lib/main.dart
@@ -1,8 +1,28 @@
+import 'dart:convert';
+
+import 'package:coincap/models/app_config.dart';
 import 'package:coincap/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get_it/get_it.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await _loadConfig();
   runApp(const MyApp());
+}
+
+Future<void> _loadConfig () async {
+
+  // load main.json from Assets folder
+  String configString = await rootBundle.loadString('assets/config/main.json');
+  Map configJson = jsonDecode(configString);
+
+  // Create singleton instance of AppConfig class with information from loaded json file in Assets folder
+  GetIt.instance.registerSingleton<AppConfig>(
+    AppConfig(COIN_API_BASE_URL: configJson['COIN_API_BASE_URL'])
+  );
 }
 
 class MyApp extends StatelessWidget {
