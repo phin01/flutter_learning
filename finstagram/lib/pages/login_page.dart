52ea15101c0926acde0aed9d84f7337402a1ef60
phin01 <phin@uol.com.br>
2022-05-23 15:21:02 -0300

    Create empty Login and Register pages

---
+++ finstagram/lib/pages/login_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(Object context) {
+    return const Scaffold();
+  }
+}
---
+++ finstagram/lib/pages/register_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<RegisterPage> createState() {
+    return _RegisterPageState();
+  }
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
