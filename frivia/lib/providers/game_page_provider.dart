c0a9cb51a21f539d772eb8bd9b5c0fe5ad47660a
phin01 <phin@uol.com.br>
2022-05-23 10:25:56 -0300

    Evaluate players answers

--- frivia/lib/providers/game_page_provider.dart
+++ frivia/lib/providers/game_page_provider.dart
@@ -12,7 +12,8 @@ class GamePageProvider extends ChangeNotifier {
   String _difficulty = 'easy';
   String _questionType = 'boolean';
 
-  late List _triviaQuestions;
+  List? triviaQuestions;
+  int _currentQuestion = 0;
 
   GamePageProvider({required this.context}) {
     _dio.options.baseUrl = 'https://opentdb.com/api.php';
@@ -32,6 +33,41 @@ class GamePageProvider extends ChangeNotifier {
     );
 
     dynamic triviaData = jsonDecode(response.toString());
-    _triviaQuestions = triviaData['results'];
+    triviaQuestions = triviaData['results'];
+    notifyListeners();
+  }
+
+  String getCurrentQuestionText() {
+    return triviaQuestions![_currentQuestion]['question'];
+  }
+
+  void answerQuestion(String answer) {
+    // compare answer string from API to provided answer from player
+    // API answer is provided in proper case, but let's compare uppercase strings
+    bool isCorrect = triviaQuestions![_currentQuestion]['correct_answer'].toUpperCase() == answer.toUpperCase();
+
+    _playerAnswerFeedback(isCorrect);
+
+    // increment current question counter and update UI
+    if(_currentQuestion == _maxQuestions - 1) {
+
+    } else {
+      _currentQuestion++;  
+    }
+    notifyListeners();
+  }
+
+  void _playerAnswerFeedback(bool isCorrect) async {
+    // show a popup with a feedback for the player regarding their answer
+    showDialog(context: context, builder: (context) {
+      return AlertDialog(
+      backgroundColor: isCorrect ? Colors.green : Colors.red,
+      title: Icon(isCorrect ? Icons.check_circle : Icons.cancel_sharp, color: Colors.white,),
+    );
+    });
+    
+    // close AlertDialog after a 1-sec delay
+    await Future.delayed(const Duration(seconds: 1));
+    Navigator.pop(context);
   }
 }
