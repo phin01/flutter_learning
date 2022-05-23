57bfe6751ab9ee49777404cb575813338352d014
phin01 <phin@uol.com.br>
2022-05-23 11:17:05 -0300

    Include aux functions

---
+++ frivia/lib/models/auxiliary_functions.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+List<DropdownMenuItem<String>> createDropDownItems(List<String> values) {
+  return values.map((String ddString) {
+      return DropdownMenuItem<String>(
+        value: ddString,
+        child: Text(ddString),
+      );
+    }
+  ).toList();
+}
+
+Future<dynamic> navigateToPage(BuildContext context, Widget destinationPage) {
+  return Navigator.push(
+    context,
+    MaterialPageRoute(
+      builder: (context) {
+        return destinationPage;
+      }
+    )
+  );
+}
