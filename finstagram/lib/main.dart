964aea80dbf31cc3aeef97ed141b85e2513a0aa7
phin01 <phin@uol.com.br>
2022-05-24 09:21:41 -0300

    Create route map

--- finstagram/lib/main.dart
+++ finstagram/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:finstagram/pages/login_page.dart';
+import 'package:finstagram/pages/register_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,11 +12,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    // route map
+    Map<String, Widget Function(BuildContext)> routeMap = {
+      'register': (context) => RegisterPage(),
+      'login': (context) => LoginPage(),
+    };
+
+    // 
     return MaterialApp(
       title: 'Finstagram',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
+      initialRoute: 'login',
+      routes: routeMap,
       home: const Scaffold(),
     );
   }
