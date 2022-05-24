5770b48a7931f93b587d4aec4ddca90f2d4a1792
phin01 <phin@uol.com.br>
2022-05-24 17:11:27 -0300

    Create blank Feed and Profile pages

---
+++ finstagram/lib/pages/feed_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Feed Page will be rendered inside HomePage, which already has a Scaffold
+class FeedPage extends StatefulWidget {
+
+  @override
+  State<FeedPage> createState() {
+    return _FeedPageState();
+  }
+}
+
+class _FeedPageState extends State<FeedPage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ finstagram/lib/pages/profile_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+// Profile Page will be rendered inside HomePage, which already has a Scaffold
+class ProfilePage extends StatefulWidget {
+  
+  @override
+  State<ProfilePage> createState() {
+    return _ProfilePageState();
+  }
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
