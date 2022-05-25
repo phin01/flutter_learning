c3a13e10dc8a593a0b0cd44747b1d1f7d27c2210
phin01 <phin@uol.com.br>
2022-05-25 13:31:16 -0300

    Implement login method

--- finstagram/lib/services/firebase_service.dart
+++ finstagram/lib/services/firebase_service.dart
@@ -2,12 +2,50 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
+// The Firebase project contains:
+//    - A email/password authentication service
+//    - A Firestore database with the following collections:
+//        - users: User data (email, name, avatar URL)
 class FirebaseService {
 
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseStorage _storage = FirebaseStorage.instance;
   FirebaseFirestore _db = FirebaseFirestore.instance;
 
+  final String usersCollection = 'users';
+  Map<String, String>? currentUser;
+
   FirebaseService();
+
+  // FirebaseAuth service uses Email/Password authentication
+  // This method returns a bool with the authentication result
+  // And also assigns the user's data to the currentUser map
+  Future<bool> userLogin({required String email, required String password}) async {
+
+    UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+
+    try {
+      if(userCredential.user != null) {
+        currentUser = await getUserData(uid: userCredential.user!.uid);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      print(e);
+      return false;
+    } 
+
+  }
+
+  // Firestore collection 'users' has 3 fields:
+  //    - (String) email
+  //    - (String) image
+  //    - (String) name
+  // This method will return the fields as a map based on the provided UID
+  Future<Map<String, String>> getUserData({required String uid}) async {
+    DocumentSnapshot doc = await _db.collection(usersCollection).doc(uid).get();
+    return doc.data() as Map<String, String>;
+  }
 
 }
