05a048c0151b623ae98e17d82cac62f33fcf7e49
phin01 <phin@uol.com.br>
2022-06-06 08:44:42 -0300

    update main.dart page

--- web_responsive/lib/main.dart
+++ web_responsive/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:web_responsive/pages/responsive_row_column.dart';
+import 'package:web_responsive/pages/responsive_wrap.dart';
 
 import 'pages/responsive_mediaquery.dart';
+import 'pages/responsive_orientation.dart';
 
 void main() {
   runApp(
     MaterialApp(
       title: 'Flutter Web',
       debugShowCheckedModeBanner: false,
-      //home: ResponsiveMediaQuery(),
-      home: ResponsiveRowColumn(),
+      // home: ResponsiveMediaQuery(),
+      // home: ResponsiveRowColumn(),
+      // home: ResponsiveWrap(),
+      home: ResponsiveOrientation(),
     )
   );
 }
