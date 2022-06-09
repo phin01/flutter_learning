0895c97fdd8ec1989f73e24aa7b102f2cc75f9a9
Paulo Nascimento <paulo.nascimento@siemens-healthineers.com>
2022-06-09 10:04:17 -0300

    Create new fb_clone project

--- fb_clone/test/widget_test.dart
+++ fb_clone/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:web_responsive/main.dart';
+import 'package:fb_clone/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
