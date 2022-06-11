ecd67a9cc78cb2fa0bb2473764837b9b7217d314
Paulo Nascimento <paulo.nascimento@siemens-healthineers.com>
2022-06-11 09:50:36 -0300

    add new post area to home page

--- fb_clone/lib/pages/home.dart
+++ fb_clone/lib/pages/home.dart
@@ -1,4 +1,7 @@
 import 'package:fb_clone/components/button_circle.dart';
+import 'package:fb_clone/components/new_post.dart';
+import 'package:fb_clone/components/stories_scroll.dart';
+import 'package:fb_clone/data/dummy_data.dart';
 import 'package:fb_clone/utils/color_palette.dart';
 import 'package:flutter/material.dart';
 import 'package:line_icons/line_icons.dart';
@@ -30,6 +33,26 @@ class _HomePageState extends State<HomePage> {
               ButtonCircle(icon: LineIcons.facebookMessenger, iconSize: 30, onPressed: (){},),
             ],
           ),
+
+          // New post area
+          SliverToBoxAdapter(
+            child: NewPostArea(currentUser: dummyCurrentUser),
+          ),
+
+          // Padding before list of Stories
+          const SliverPadding(padding: EdgeInsets.fromLTRB(0, 10, 0, 5)),
+
+          // List of Stories
+          SliverToBoxAdapter(
+            child: StoriesScroll(currentUser: dummyCurrentUser, stories: dummyStories),
+          ),
+
+          SliverToBoxAdapter(
+            child: Container(height: 500, color: Colors.green,),
+          ),
+
+
+
         ],
       ),
     );
